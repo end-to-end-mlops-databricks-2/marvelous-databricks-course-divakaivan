240d89c8065159264589e3a53b71da2606862437
divakaivan <timee98@outlook.com>
2025-02-04 12:03:04 +0900

    add ProjectConfig class

---
+++ src/bank_marketing/config.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import List, Dict
+import yaml
+
+class ProjectConfig(BaseModel):
+    """Project configuration"""
+
+    raw_data_schema: Dict[str, str]
+    catalog_name: str
+    schema_name: str
+    train_set_name: str
+    test_set_name: str
+    num_features: List[str]
+    cat_features: List[str]
+    target: str
+    
+    @classmethod
+    def from_yaml(cls, config_path: str):
+        """Load configuration from yaml file"""
+        with open(config_path, 'r') as f:
+            config_dict = yaml.safe_load(f)
+        return cls(**config_dict)
